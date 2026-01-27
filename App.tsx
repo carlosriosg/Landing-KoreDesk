ca1981cc33d1410d0256db8e940301e293bdba6f
carlosriosg <carlosriosg@gmail.com>
2026-01-27 12:36:49 -0300

    Switch from HashRouter to BrowserRouter
    Replaced HashRouter with BrowserRouter and added a console log for debugging.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,25 @@
-
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import TermsPage from './pages/TermsPage';
 
 const App: React.FC = () => {
+    // Agregamos un console.log para ver si App se está ejecutando
+    console.log("App component mounted!"); 
+
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Routes>
+                {/* Ruta principal */}
                 <Route path="/" element={<HomePage />} />
+                
+                {/* Ruta de términos */}
                 <Route path="/terminos" element={<TermsPage />} />
+                
+                {/* Ruta de prueba para confirmar que no es la Home la que falla */}
+                <Route path="/test" element={<h1 style={{color: 'red', fontSize: '50px'}}>FUNCIONA</h1>} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     );
 };
 
